de383f1423839133e61c7a213f0a2b98d52b8258
Rojan Dinc <rojand94@gmail.com>
2019-01-21 19:10:35 +0100

    Test new post types

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/services`,
+        path: `${__dirname}/posts`,
         name: 'markdown-pages',
       },
     },
