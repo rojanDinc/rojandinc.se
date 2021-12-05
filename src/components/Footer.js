951fa13418ee381303614d1b28d029d6e2960537
rojan dinc <rojand94@gmail.com>
2021-12-06 00:41:54 +0100

    Update footer jsx

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,18 +6,16 @@ const Footer = () => {
 
   return (
     <footer>
-      <Container>
-        <Row>
-          <Col xs={12}>
-            <hr style={{color: "var(--dark-primary)"}} />
-          </Col>
-          <Col xs={12}>
-            <div className="d-flex justify-content-center my-5">
-              &#169; {currentYear}
-            </div>
-          </Col>
-        </Row>
-      </Container>
+      <Row>
+        <Col xs={12}>
+          <hr style={{ color: "var(--dark-primary)" }} />
+        </Col>
+        <Col xs={12}>
+          <div className="d-flex justify-content-center my-5">
+            rojandinc.se &#169; {currentYear}
+          </div>
+        </Col>
+      </Row>
     </footer>
   );
 };
