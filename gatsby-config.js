7b590006987b56e04c40edb681e6f6b9fb0ad4ab
rojan dinc <rojand94@gmail.com>
2021-12-06 00:00:56 +0100

    Add markdown plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,10 +33,23 @@ module.exports = {
       __key: "pages",
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name: "posts",
-        path: "./src/pages/posts/",
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 1920,
+            },
+          },
+        ],
       },
     },
   ],
