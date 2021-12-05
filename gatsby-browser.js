32bed866edb58b3f94dfaab31158d369fb5d7260
rojan dinc <rojand94@gmail.com>
2021-12-06 00:41:34 +0100

    Update bootstrap css to full import

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,5 +3,5 @@ import "@fontsource/poppins/500-italic.css";
 import "@fontsource/poppins/600.css";
 import "@fontsource/poppins/700.css";
 import "@fontsource/poppins/800.css";
-import "bootstrap/dist/css/bootstrap-grid.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./src/styles/global.css";
