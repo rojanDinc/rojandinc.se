abb06de1b8ab9bf977a88a1dd7ebc25f6a9b9ba1
rojan dinc <rojand94@gmail.com>
2021-12-08 01:28:54 +0100

    Update blog page code structure

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,11 @@
 import * as React from "react";
-import { graphql } from "gatsby";
-import { Container, Row, Col } from "react-bootstrap";
-import { Card, Navbar } from "../components";
+import { graphql, Link } from "gatsby";
+import { Row, Col } from "react-bootstrap";
+import { Card, Layout, Divider } from "../components";
+import { groupBy } from "../util";
+import styled from "styled-components";
 
-class Post {
+class PostModel {
   constructor(slug, title, postDate) {
     this.slug = slug;
     this.title = title;
@@ -11,39 +13,62 @@ class Post {
   }
 
   static createPost(obj) {
-    return new Post(obj.slug, obj.title, new Date(obj.date));
+    return new PostModel(obj.slug, obj.title, new Date(obj.date));
   }
 }
 
-export default function Blog({ data }) {
-  console.log(data);
-  const posts = data.allMarkdownRemark.edges
-    .map((edge) => Post.createPost(edge.node.frontmatter))
-    .sort((a, b) => b.postDate.getTime() - a.postDate.getTime());
+const PostDate = styled.div`
+  text-decoration: none;
+`;
 
-  const renderPost = (post, i) => (
-    <Col key={i} xs={12}>
-      <a href={post.slug}>
-        <div>
-          {post.title} - {post.postDate.toLocaleDateString()}
-        </div>
-      </a>
-    </Col>
-  );
+const Post = ({ post }) => (
+  <Col xs={12} className="mb-3">
+    <Link href={post.slug}>
+      <Card className="p-3 d-flex flex-row justify-content-between">
+        <div>{post.title}</div>
+        <PostDate>{post.postDate.toLocaleDateString()}</PostDate>
+      </Card>
+    </Link>
+  </Col>
+);
+
+const PostSection = ({ section }) => {
+  const [year, collection] = section;
+  const sortPosts = (a, b) => b.postDate.getTime() - a.postDate.getTime();
 
   return (
-    <React.Fragment>
-      <Navbar />
-      <Container>
-        <Row>
-          <Col xs={12}>
-            <h1>Blog</h1>
-            <p>Lorem ipsum dolor sit.</p>
-          </Col>
-        </Row>
-        <Row>{posts.map(renderPost)}</Row>
-      </Container>
-    </React.Fragment>
+    <Row key={`${year}`} className="mb-4">
+      <Col>
+        <h3>{year}</h3>
+      </Col>
+      {collection.sort(sortPosts).map((post, i) => (
+        <Post key={i} post={post} />
+      ))}
+    </Row>
+  );
+};
+
+export default function Blog({ data }) {
+  const posts = data.allMarkdownRemark.edges.map((edge) =>
+    PostModel.createPost(edge.node.frontmatter)
+  );
+  const groupedPosts = groupBy(posts, (post) => post.postDate.getFullYear());
+  const sortSections = (a, b) => b[0] - a[0];
+
+  return (
+    <Layout>
+      <Row>
+        <Col xs={12}>
+          <h1 className="font-bold">Blog posts</h1>
+          <Divider />
+        </Col>
+      </Row>
+      {Object.entries(groupedPosts)
+        .sort(sortSections)
+        .map((section) => (
+          <PostSection section={section} />
+        ))}
+    </Layout>
   );
 }
 
