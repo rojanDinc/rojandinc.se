e64221d189183cfe445d1fc1ec71c6b7bafcdc37
rojan dinc <rojand94@gmail.com>
2022-01-06 21:49:41 +0100

    Update site font to monospace

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,2 @@
-import "@fontsource/poppins/500.css";
-import "@fontsource/poppins/500-italic.css";
-import "@fontsource/poppins/600.css";
-import "@fontsource/poppins/700.css";
-import "@fontsource/poppins/800.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./src/styles/global.css";
