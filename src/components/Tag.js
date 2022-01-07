4e9e383e9b119592fb04163e469f7b3ce6d2035f
rojan dinc <rojand94@gmail.com>
2022-01-07 04:10:51 +0100

    Update tag component

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -2,19 +2,18 @@ import * as React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
-const StyledText = styled.span`
+const StyledText = styled.div`
   background-color: #f2f2f2;
   color: #222;
   font-weight: 700;
-  padding: 0.75rem;
+  padding: 0.65rem 1.15rem;
   border-radius: 0.75rem;
+  display: inline-block;
+  margin-bottom: 0.5rem;
+  margin-right: 0.5rem;
 `;
 
-const Tag = ({ text }) => (
-  <div>
-    <StyledText>{text}</StyledText>
-  </div>
-);
+const Tag = ({ text }) => <StyledText>{text}</StyledText>;
 
 Tag.propTypes = {
   text: PropTypes.string.isRequired,
