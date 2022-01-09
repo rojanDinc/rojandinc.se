83dbd63fc27d2aa18d98ac0ec6921cb6d59926f3
rojan dinc <rojand94@gmail.com>
2022-01-09 02:34:41 +0100

    Update navbar to use css shadow

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,8 +10,16 @@ const NavbarBrand = styled(Link)`
   }
 `;
 
+const CustomNavbar = styled(Navbar)`
+  background-color: var(--primary);
+  --tw-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+    0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000),
+    var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
+`;
+
 const _Navbar = () => (
-  <Navbar
+  <CustomNavbar
     collapseOnSelect
     className="navbar mb-5 py-3"
     expand={false}
@@ -31,7 +39,7 @@ const _Navbar = () => (
         </Nav>
       </Navbar.Collapse>
     </Container>
-  </Navbar>
+  </CustomNavbar>
 );
 
 export { _Navbar as Navbar };
