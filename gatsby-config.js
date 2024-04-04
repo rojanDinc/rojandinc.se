1d83c4eb691a2ece64952b7a3cdd3f63939bb82e
rojanDinc <rojand94@gmail.com>
2024-04-04 22:50:38 +0200

    remove gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,23 +53,5 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "G-467H1W5E1Q", // Google Analytics / GA
-        ],
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          // Defaults to https://www.googletagmanager.com
-        },
-      },
-    },
   ],
 };
