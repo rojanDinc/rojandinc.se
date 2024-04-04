9468858588d6585bc56928f1cba0061700875923
Rojan Dinc <rojand94@gmail.com>
2024-04-05 00:43:59 +0200

    Add docker file (#2)
    * remove gtag
    
    * add gatsby node js
    
    * add server file
    
    * add docker file

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-styled-components`,
+    "gatsby-plugin-nodejs",
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
@@ -51,24 +52,6 @@ module.exports = {
             },
           },
         ],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          "G-467H1W5E1Q", // Google Analytics / GA
-        ],
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          // Defaults to https://www.googletagmanager.com
-        },
       },
     },
   ],
